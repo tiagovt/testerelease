d2813fd22ff9bae5aa6c7459fc3f0756e860ce6b
Your Name <you@example.com>
2018-09-19 09:20:37 -0300

    first commit

---
+++ src/main/java/br/com/testerelease/testerelease/TestereleaseApplication.java
@@ -0,0 +1,12 @@
+package br.com.testerelease.testerelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestereleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestereleaseApplication.class, args);
+	}
+}
